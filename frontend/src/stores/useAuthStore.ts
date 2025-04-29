2d3595d97799a78086cde49126f6aeda07decf9b
DJ <gk22719@gmail.com>
2025-04-29 23:56:44 +0530

    fixed bugs

--- frontend/src/stores/useAuthStore.ts
+++ frontend/src/stores/useAuthStore.ts
@@ -26,7 +26,7 @@ interface AuthState {
 
 const useAuthStore = create<AuthState>()(
   persist(
-    (set, get) => ({
+    (set) => ({
       user: null,
       token: null,
       isAuthenticated: false,
--- frontend/src/stores/usePodcastStore.ts
+++ frontend/src/stores/usePodcastStore.ts
@@ -39,7 +39,7 @@ interface PodcastState {
   clearError: () => void;
 }
 
-const usePodcastStore = create<PodcastState>((set, get) => ({
+const usePodcastStore = create<PodcastState>((set) => ({
   podcasts: [],
   episodes: [],
   currentPodcast: null,
